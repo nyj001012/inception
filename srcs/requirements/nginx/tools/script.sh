1b6b6e623efa8a43600924a813d863bf9e97a4a5
root <root@yena.42seoul.kr>
2023-09-14 17:12:14 +0900

    feat: remove CERTS, KEYS from .env

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-SSL_PATH=/etc/ssl/
-KEY_NAME=${SSL_PATH}/private/nginx.key
-CRT_NAME=${SSL_PATH}/certs/nginx.crt
+CERTS=/etc/ssl/certs/nginx.crt
+KEYS=/etc/ssl/private/nginx.key
 
-openssl req -newkey rsa:2048 -nodes -x509 -keyout ${KEY_NAME} -out ${CRT_NAME} -days 365 \
-	-subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/CN=localhost"
-
-chown nginx:nginx ${KEY_NAME} ${CRT_NAME}
-chmod 600 ${KEY_NAME} ${CRT_NAME}
+openssl req -newkey rsa:2048 -nodes -x509 -keyout ${KEYS} -out ${CERTS} -days 365 -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/CN=yena.42.fr"
